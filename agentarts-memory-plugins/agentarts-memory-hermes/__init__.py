3f7c396f89f0cd2663626479cd55ad8cb3f7b79b
weiyang <gglzf4@gmail.com>
2026-08-08 22:36:14 +0800

    feat(memory): add hermes memory plugin

---
+++ agentarts-memory-plugins/agentarts-memory-hermes/__init__.py
@@ -0,0 +1,7 @@
+"""AgentArts Memory Provider plugin for Hermes Agent."""
+
+from .provider import AgentArtsMemoryProvider, register
+
+__version__ = "1.0.0"
+
+__all__ = ["AgentArtsMemoryProvider", "register"]
